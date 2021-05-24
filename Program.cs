8fa2cacf5616b2102bc1684971569c2d367a69e4
Ademodi ifedayo <ademodiseun@gmail.com>
2021-05-24 14:01:04 +0100

    firstcommit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DemoAccesModifiers
+{
+    class Program
+    {
+
+        // The application print the name of a particular user workspace..
+        static void Main(string[] args)
+        {
+            UserName  user1 = new UserName();
+		     user1.printUserName();
+            // Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class User
+     {
+	private string Name ="IFE";
+
+	protected string display()
+	{
+		return Name;
+	}
+}
+public class UserName: User
+{
+	public string  name;
+	public void printUserName()
+	{
+		name = display();
+		Console.WriteLine("The username : " + name);
+	}
+
+}
+
+}
